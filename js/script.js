dd1ef8e71120e59f61486e1912b08af188aced5c
Matthieu34400 <matthieu.verquera@gmail.com>
2016-06-27 17:00:35 +0200

    Ajout de la page d'accueil avec barre de chargement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-document.querySelector("#bouton").onclick = function() {
-if (window.getComputedStyle(document.querySelector('#section-contact')).display=='none'){
-document.querySelector("#section-contact").style.display="block";
-} else {
-document.querySelector("#section-contact").style.display="none";
-}
-}
+$(document).ready(function() {
+
+        var MaBarreProgression = $('#MaBarreProgression'),
+            max = MaBarreProgression.attr('max'),
+            time = 100,
+            value = MaBarreProgression.val();
+
+        var Telechargement = function() {
+            value += 4;
+            addValue = MaBarreProgression.val(value);
+
+            $('.progress-value').html('Chargement de la page... '+ value + '%');
+
+            if (value == max) {
+                clearInterval(animation);
+                $('.progress-value').html('Chargement Terminé !');
+            }
+        };
+
+        var animation = setInterval(function() {
+            Telechargement();
+        }, time);
+
+});
+
+$(document).ready(function()
+{
+   // On cache la zone de texte
+    $('#bouton').hide();
+    $('#bouton').show(function()
+  {
+    $('#bouton').show(100);
+      return true;
+
+   });
+});
