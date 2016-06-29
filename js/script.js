a9a9457f9efcba0c6d28df302d7ab88ace0fbfad
Matthieu34400 <matthieu.verquera@gmail.com>
2016-06-29 11:19:15 +0200

    Changement fond d'ecran et police

--- js/script.js
+++ js/script.js
@@ -1,42 +1,14 @@
+//Fonction qui permet de faire les transitions
 $(document).ready(function() {
 
-        var MaBarreProgression = $('#MaBarreProgression'),
-            max = MaBarreProgression.attr('max'),
-            time = 100,
-            value = MaBarreProgression.val();
-
-        var Telechargement = function() {
-            value += 4;
-            addValue = MaBarreProgression.val(value);
-
-            $('.progress-value').html('Chargement de la page... '+ value + '%');
-
-            if (value == max) {
-                clearInterval(animation);
-                $('.progress-value').html('Chargement terminé !');
-            }
-        };
-
-        var animation = setInterval(function() {
-            Telechargement();
-        }, time);
-
-});
-
-$(document).ready(function() {
-
-	$("body").css("display", "none");
-
+	  $("body").css("display", "none");
     $("body").fadeIn(3700);
-
-	$("a.transition").click(function(event){
-		event.preventDefault();
-		linkLocation = "index2.html".href;
-		$("body").fadeOut(3000, redirectPage);
+    $("a.transition").click(function(event){
+  		event.preventDefault();
+  		$("body").fadeOut(3000, redirectPage);
 	});
-
-	function redirectPage() {
+  
+  function redirectPage() {
 		window.location = linkLocation;
 	}
-
 });
